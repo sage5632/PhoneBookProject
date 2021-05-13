ccc8b82f58ba73751c01aa225e3e2f63b1bed1b1
박정환 <sage5632@gmail.com>
2021-05-13 09:50:52 +0900

    select

--- 01-SELECT.sql
+++ 01-SELECT.sql
@@ -94,6 +94,61 @@ SELECT * FROM employees
 WHERE department_id = 10;
 
 -- 논리 조합
+-- 급여가 14000 이하 or 17000 이상인 사원의 이름과 급여
+
+SELECT first_name, salary
+FROM employees
+WHERE salary <= 14000 OR salary >= 17000;
+
+-- 여집합
+SELECT first_name, salary
+FROM employees
+WHERE NOT (salary <= 14000 OR salary >= 17000);
+
+-- 부서 아이디가 90이고 급여 => 20000
+SELECT * FROM employees
+WHERE DEPARTMENT_ID = 90 AND salary >= 20000;
+
+-- 입사일이 07/01/01 ~ 07/12/31 구간의 모든 사원
+SELECT first_name, hire_date
+FROM employees
+WHERE hire_date between '07/01/01' AND '07/12/31';
+
+-- IN 연산자
+SELECT * FROM employees
+WHERE department_id IN (10, 20, 40);
+
+-- manager id가 100, 120, 147인 사원의 명단
+SELECT * FROM employees
+-- WHERE MANAGER_ID IN (100, 120, 147);
+WHERE manager_id = 100 OR manager_id = 120 or manager_id = 147;
+
+-- LIKE 검색
+-- % : 임의의 길이의 지정되지 않은 문자열
+-- _ : 한개의 임의의 문자
+
+--이름에 am을 포함한 사원의 이름과 급여
+SELECT first_name, salary
+FROM employees
+WHERE first_name LIKE '%am%';
+
+-- 이름의 두 번쨰 글자가 a인 사원의 이름과 급여
+SELECT first_name, salary
+FROM employees
+WHERE first_name LIKE '_a%';
+
+--이름의 네 번째 글자가 a인 사원의 이름과 급여
+SELECT first_name, salary
+FROM employees
+WHERE first_name LIKE '___a%';
+
+-- 이름이 4글자인 사원 중에 끝에서 두번쨰 글자가 a인 사원의 이름과 급여
+SELECT first_name, salary
+FROM employees
+WHERE first_name LIKE '__a_';
+
+
+
 
 
 
